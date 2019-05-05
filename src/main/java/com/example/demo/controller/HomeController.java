66c51152221324146d8d9871a00d7d4ad6051cb2
Naresh G L <glnaresh@Nareshs-MBP.fritz.box>
2019-05-05 19:37:25 +0200

    removing unused imports

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -3,7 +3,6 @@
  */
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
