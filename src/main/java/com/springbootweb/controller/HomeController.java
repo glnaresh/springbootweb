b33a0cf976368357b8dbadd48d48a248e85df16a
Naresh Ganapavaram Lakshmi <glnaresh@gmail.com>
2019-05-08 13:54:02 +0200

    changing the package for home controller

--- src/main/java/com/springbootweb/controller/HomeController.java
+++ src/main/java/com/springbootweb/controller/HomeController.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.example.demo.controller;
+package com.springbootweb.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
